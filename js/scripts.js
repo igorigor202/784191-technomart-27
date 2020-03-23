b4cdccd214d597fd6e1cf3f2446cdf469b8a7c0c
Игорь <igorigorcs2@gmail.com>
2020-03-24 01:07:13 +0300

    MAIN CATALOG V4

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 $(function () {
-  $('.popup-modal').magnificPopup({
-    type: 'inline',
-    preloader: false,
-    focus: '#username',
-    modal: true
-  });
-  $(document).on('click', '.popup-modal-dismiss', function (e) {
-    e.preventDefault();
-    $.magnificPopup.close();
-  });
+	$('.popup-modal').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#username',
+		modal: true
+	});
+	$(document).on('click', '.popup-modal-dismiss', function (e) {
+		e.preventDefault();
+		$.magnificPopup.close();
+	});
 });
 
